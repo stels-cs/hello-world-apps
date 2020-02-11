76e304d7ceca919da53d57c82ef22382abf5e60a
roma-php <52506215+roma-php@users.noreply.github.com>
2020-02-11 22:19:07 +0300

    уу
    уу

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Panel, PanelHeader, HeaderButton, platform, IOS} from '@vkontakte/vkui';
-import persik from '../img/persik.png';
-import './Persik.css';
-import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-const osname = platform();
+    this.state = {
+      activeView: 'view1'
+    }
+  }
 
-const Persik = props => (
-	<Panel id={props.id}>
-		<PanelHeader
-			left={<HeaderButton onClick={props.go} data-to="home">
-				{osname === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</HeaderButton>}
-		>
-			Persik
-		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
-	</Panel>
-);
+  render() {
+    return (
+      <Root activeView={this.state.activeView}>
+        <View activePanel="panel1.1" id="view1">
+          <Panel id="panel1.1">
+            <PanelHeader>View 1</PanelHeader>
+            <Group>
+              <CellButton onClick={ () => this.setState({ activeView: 'view2' }) }>
+                Open View 2
+              </CellButton>
+            </Group>
+          </Panel>
+        </View>
+        <View header activePanel="panel2.1" id="view2">
+          <Panel id="panel2.1">
+            <PanelHeader>View 2</PanelHeader>
+            <Group>
+              <CellButton onClick={ () => this.setState({ activeView: 'view1' }) }>
+                Back to View 1
+              </CellButton>
+            </Group>
+          </Panel>
+        </View>
+      </Root>
+    )
+  }
+}
 
-Persik.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-};
-
-export default Persik;
+<Example />
